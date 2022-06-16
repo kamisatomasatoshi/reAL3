64c200c9d4a2808ca26ef445bd15f321bea75e9a
kamisatomasatoshi <k021g1271@g.neec.ac.jp>
2022-06-16 13:17:10 +0900

    プロジェクト ファイルを追加します。

---
+++ scene/GameScene.cpp
@@ -0,0 +1,126 @@
+﻿#include "GameScene.h"
+#include "TextureManager.h"
+#include "AxisIndicator.h"
+#include "PrimitiveDrawer.h"
+#include "afin.h"
+#include <cassert>
+
+GameScene::GameScene() {}
+
+GameScene::~GameScene()
+{
+	delete model_;
+	delete debugCamera_;
+}
+
+void GameScene::Initialize()
+{
+
+	dxCommon_ = DirectXCommon::GetInstance();
+	input_ = Input::GetInstance();
+	audio_ = Audio::GetInstance();
+	debugText_ = DebugText::GetInstance();
+
+	textureHandle_ = TextureManager::Load("mario.jpg");
+	model_ = Model::Create();
+
+	//ワールドトランスフォーム
+	worldTransform_.Initialize();
+	//ビュープロダクション
+	viewProjection_.Initialize();
+
+	//デバックカメラの生成
+	debugCamera_ = new DebugCamera(WinApp::kWindowWidth, WinApp::kWindowHeight);
+	//軸方向表示の表示を有効化する
+	AxisIndicator::GetInstance()->SetVisible(TRUE);
+	//軸方向表示が参照するビュープロダクションを指定する（アドレス渡し）
+	AxisIndicator::GetInstance()->SetTargetViewProjection(&debugCamera_->GetViewProjection());
+
+	//ライン描画が参照するビュープロダクションを指定する(アドレス渡し）
+	PrimitiveDrawer::GetInstance()->SetViewProjection(&debugCamera_->GetViewProjection());
+
+
+
+
+	worldTransform_.scale_ = { 2.0f, 2.0f, 2.0f };
+
+
+
+	//worldTransform_.matWorld_ = {
+	//	1.0f, 0.0f, 0.0f, 0.0f,
+	//	1.0f, 1.0f, 0.0f, 0.0f,
+	//	1.0f, 0.0f, 1.0f, 0.0f,
+	//	1.0f, 0.0f, 0.0f, 1.0f
+	//};
+	//worldTransform_.matWorld_ *= matScale;
+
+
+
+	Matrix4 matScale = genScaleMatrix(worldTransform_.scale_); // (1)
+	setMatWolrd(worldTransform_, matScale);
+	//行列の転送
+	worldTransform_.TransferMatrix();
+
+}
+
+void GameScene::Update()
+{
+	debugCamera_->Update();
+}
+
+void GameScene::Draw()
+{
+
+	// コマンドリストの取得
+	ID3D12GraphicsCommandList* commandList = dxCommon_->GetCommandList();
+
+#pragma region 背景スプライト描画
+	// 背景スプライト描画前処理
+	Sprite::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに背景スプライトの描画処理を追加できる
+	/// </summary>
+
+	// スプライト描画後処理
+	Sprite::PostDraw();
+	// 深度バッファクリア
+	dxCommon_->ClearDepthBuffer();
+#pragma endregion
+
+#pragma region 3Dオブジェクト描画
+	// 3Dオブジェクト描画前処理
+	Model::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに3Dオブジェクトの描画処理を追加できる
+	/// </summary>
+	model_->Draw(worldTransform_, debugCamera_->GetViewProjection(), textureHandle_);
+	for (int i = 0; i < 8; i++)
+	{
+		//PrimitiveDrawer::GetInstance()->DrawLine3d(line[i], line[i+1 % 8], color);
+		PrimitiveDrawer::GetInstance()->DrawLine3d(startLine[i], endLine[i], color);
+
+	}
+	// 3Dオブジェクト描画後処理
+	Model::PostDraw();
+#pragma endregion
+
+#pragma region 前景スプライト描画
+	// 前景スプライト描画前処理
+	Sprite::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに前景スプライトの描画処理を追加できる
+	/// </summary>
+
+	// デバッグテキストの描画
+	debugText_->DrawAll(commandList);
+	//
+	// スプライト描画後処理
+	Sprite::PostDraw();
+
+#pragma endregion
+}
+
+
---
+++ scene/GameScene.h
@@ -0,0 +1,130 @@
+﻿#pragma once
+
+#include "Audio.h"
+#include "DirectXCommon.h"
+#include "DebugText.h"
+#include "Input.h"
+#include "Model.h"
+#include "SafeDelete.h"
+#include "Sprite.h"
+#include "ViewProjection.h"
+#include "WorldTransform.h"
+#include "DebugCamera.h"
+
+/// <summary>
+/// ゲームシーン
+/// </summary>
+class GameScene {
+
+  public: // メンバ関数
+	/// <summary>
+	/// コンストクラタ
+	/// </summary>
+	GameScene();
+
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
+	~GameScene();
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
+	/// 毎フレーム処理
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw();
+
+  private: // メンバ変数
+	DirectXCommon* dxCommon_ = nullptr;
+	Input* input_ = nullptr;
+	Audio* audio_ = nullptr;
+	DebugText* debugText_ = nullptr;
+
+	/// <summary>
+	/// ゲームシーン用
+	uint32_t textureHandle_ = 0;
+	//3Dモデルの生成
+	Model* model_ = nullptr;
+	DebugCamera* debugCamera_ = nullptr;
+
+	
+
+	Vector3 box[8] = {
+		{0.0f, 0.0f, 0.0f},//0
+		{5.0f, 0.0f, 0.0f},//1
+		{5.0f, 0.0f, 5.0f},//2
+		{0.0f, 0.0f, 5.0f},//3
+
+		{0.0f, 5.0f, 0.0f},//4
+		{5.0f, 5.0f, 0.0f},//5
+		{5.0f, 5.0f, 5.0f},///6
+		{0.0f, 5.0f, 5.0f},//7
+	};
+
+	Vector3 startLine[8] = {
+		//縦
+		{-7.5f, 0.0f, -7.5f},//0
+		{-2.5f, 0.0f, -7.5f},//1
+		{2.5f, 0.0f, -7.5f},//2
+		{7.5f, 0.0f, -7.5f},//3
+		//横
+		{-7.5f, 0.0f, -7.5f},//4
+		{-7.5f, 0.0f, -2.5f},//5
+		{-7.5f, 0.0f, 2.5f},///6
+		{-7.5f, 0.0f, 7.5f},//7
+	};
+
+	Vector3 endLine[8] = {
+		//縦
+		{-7.5f, 0.0f, 7.5f},//0
+		{-2.5f, 0.0f, 7.5f},//1
+		{2.5f, 0.0f, 7.5f},//2
+		{7.5f, 0.0f, 7.5f},//3
+		//横
+		{7.5f, 0.0f, -7.5f},//4
+		{7.5f, 0.0f, -2.5f},//5
+		{7.5f, 0.0f, 2.5f},///6
+		{7.5f, 0.0f, 7.5f},//7
+	};
+
+
+
+
+	int boxEdgeList[12][2] =
+	{
+		{0,1},
+		{1,2},
+		{2,3},
+		{3,0},
+		{0,4},
+		{1,5},
+		{2,6},
+		{3,7},
+		{4,5},
+		{5,6},
+		{6,7},
+		{7,4},
+	};
+
+	//色の設定
+	Vector4 color = {
+		0xFF, 0xFF, 0xFF, 0xFF
+	};
+	
+	
+
+	//ワールドトランスフォーム
+	WorldTransform worldTransform_;
+	//ビュープロダクション
+	ViewProjection viewProjection_;
+
+	/// </summary>
+};
